a54d48cfa1e620501443688bdfa9f61d5db6f505
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2024-01-03 18:46:43 -0500

    feat: add components

--- packages/cv-astro/index.ts
+++ packages/cv-astro/index.ts
@@ -1,6 +1,23 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro';
+import Cv from './src/Cv.astro';
 
-export default MyComponent;
+export default Cv;
+
+export type {
+  ICvData,
+  ICertification,
+  IEducation,
+  IExperience,
+  ILanguage,
+  IProject,
+  ISkill,
+  IOrganization,
+  IPersonalInfo,
+  IPosition,
+  ITask,
+  IVolunteer,
+  IVolunteerPosition,
+  ECvSection
+} from './src/cv';
---
+++ packages/cv-astro/src/cv.d.ts
@@ -0,0 +1,119 @@
+export interface ICvData {
+  info: IPersonalInfo;
+  experience?: IExperience[];
+  volunteering?: IVolunteer[];
+  education?:  IEducation[];
+  certifications?: ICertification[];
+  skills?: ISkill[];
+  projects?: IProject[];
+  order?: ECvSection[];
+}
+
+export enum ECvSection {
+  Info = 'info',
+  Experience = 'experience',
+  Volunteering = 'volunteering',
+  Education = 'education',
+  Certifications = 'certifications',
+  Skills = 'skills',
+  Projects = 'projects',
+}
+
+export interface IPersonalInfo {
+  name: string;
+  title: string;
+  email: string;
+  phone: string;
+  website: string;
+  linkedin: string;
+  github: string;
+  twitter: string;
+  address: string;
+  timezone: string;
+  addressUrl: string;
+  summary: string;
+}
+
+export interface IExperience {
+  company: IOrganization;
+  current?: boolean;
+  positions: IPosition[];
+}
+
+export interface IPosition {
+  title: string;
+  startDate: string;
+  endDate?: string;
+  description: string;
+  tasks: ITask[];
+  projects?: IProject[];
+}
+
+export interface IVolunteer {
+  organization: IOrganization;
+  description?: string;
+  positions: IVolunteerPosition[];
+  tasks: string[];
+  isActiveMember: boolean;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface IVolunteerPosition {
+  title: string;
+  startDate: string;
+  endDate?: string;
+}
+
+export interface ITask {
+  description: string;
+  output?: string;
+}
+
+export interface IProject {
+  name: string;
+  url?: string;
+  description: string;
+  output?: string;
+  technologies: string[];
+}
+
+export interface IEducation {
+  institution: IOrganization;
+  degree: string;
+  major?: string;
+  startDate: string;
+  endDate: string;
+  projects?: IProject[];
+  achievements?: string[];
+}
+
+export interface IOrganization {
+  name: string;
+  city?: string;
+  country: string;
+  url?: string;
+  remarks?: string;
+}
+
+export interface ICertification {
+  authority: string;
+  name: string;
+  code?: string;
+  description?: string;
+  license?: string;
+  issueDate: string;
+  expirationDate?: string;
+  url?: string;
+  image?: string;
+}
+
+export interface ILanguage {
+  name: string;
+  proficiency: string;
+}
+
+export interface ISkill {
+  name: string;
+  proficiency: number;
+}
