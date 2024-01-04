687a039716488409ba67e8a7b8cad7437ba018a4
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2024-01-04 01:06:13 -0500

    chore(demo): add cv-astro source to tailwind.config.ts

---
+++ demo/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/cv-astro/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
