32574473dc708fd74d66f99e362907544ce27869
Ricardo Tribaldos <ricardo@tribal2.dev>
2024-11-09 21:53:12 -0500

    chore: update demo data, fix imports and add project CONTRIBUTING guide

--- demo/tailwind.config.ts
+++ demo/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/cv-astro/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+		'../**/cv-astro/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
   theme: {
     extend: {},
